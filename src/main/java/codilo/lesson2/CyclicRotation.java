ab4570d8f0880ae6cfb502e698977d787d185576
Vitaliy Normatov <vnormatov@gmail.com>
2019-01-20 16:35:52 +0200

    added lesson 2 task2

--- src/main/java/codilo/lesson2/CyclicRotation.java
+++ src/main/java/codilo/lesson2/CyclicRotation.java
@@ -1,4 +1,16 @@
 package codilo.lesson2;
 
-public class CyclicRotation {
+class CyclicRotation {
+
+    int[] solution(int[] A, int K) {
+        int length = A.length;
+        for (int j = 0; j < K; j++) {
+            int lastElement = A[length - 1];
+            for (int i = length - 1; i > 0; i--) {
+                A[i] = A[i - 1];
+            }
+            A[0] = lastElement;
+        }
+        return A;
+    }
 }
--- src/test/java/codilo/lesson2/CyclicRotationTest.java
+++ src/test/java/codilo/lesson2/CyclicRotationTest.java
@@ -1,5 +1,39 @@
 package codilo.lesson2;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+//https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
 public class CyclicRotationTest {
+    @Test(dataProvider = "testData")
+    public void test(int[] array, int[] arrayExpected, int r) {
+        CyclicRotation cyclicRotation = new CyclicRotation();
+        int[] arrayActual = cyclicRotation.solution(array, r);
+        Assert.assertEquals(arrayActual, arrayExpected, Arrays.toString(arrayActual));
+    }
+
+    @DataProvider
+    public Object[][] testData() {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] aR = {5, 1, 2, 3, 4};
+        int ar = 1;
+
+        int[] b = {1, 2, 3, 4, 5};
+        int[] bR = {4, 5, 1, 2, 3};
+        int br = 2;
+
+        int[] c = {1, 2, 3, 4, 5};
+        int[] cR = {3, 4, 5, 1, 2};
+        int cr = 3;
+
+        return new Object[][]{
+                {a, aR, ar},
+                {b, bR, br},
+                {c, cR, cr},
+        };
+    }
 
 }
